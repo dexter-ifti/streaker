17f39530cddfe55db059144651824cdf9195c807
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2026-01-31 21:16:02 +0530

    feat: Specify HS256 algorithm for JWT middleware and create a new `fetures_to_add.md` file.

--- streaker-backend/src/middleware/auth.middleware.ts
+++ streaker-backend/src/middleware/auth.middleware.ts
@@ -3,6 +3,7 @@ import { jwt } from "hono/jwt";
 
 export const authMiddleware = async (c : Context, next : Next) => {
     return jwt({
-        secret : c.env.JWT_SECRET
+        secret : c.env.JWT_SECRET,
+        alg : "HS256"
     })(c, next);
 }
